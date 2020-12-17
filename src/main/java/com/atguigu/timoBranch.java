9ff862cce648a3c17f9bf43e068a22aa000561bb
zm8383 <838309006@qq.com>
2020-12-17 11:00:50 +0800

    原版

---
+++ src/main/java/com/atguigu/timoBranch.java
@@ -0,0 +1,8 @@
+package com.atguigu;
+
+public class timoBranch {
+    public static void main(String[] args) {
+        System.out.println("提莫队长正在待命");
+        System.out.println("一 二 三 四");
+    }
+}
